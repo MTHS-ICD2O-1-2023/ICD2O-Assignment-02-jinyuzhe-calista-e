14a4448f85649e0deac6a6f65ca6e244dc6f01a6
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-03-26 00:53:38 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a parallaelogram.
+ */
+function calculateAreaOfParallaelogram () {
+  // input
+  const baseOfParallaelogram = parseInt(document.getElementById('base-of-parallaelogram').value)
+  const heightOfParallaelogram = parseInt(document.getElementById('height-of-parallaelogram').value)
+
+  // process
+  const areaOfParallaelogram = baseOfParallaelogram * heightOfParallaelogram
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfParallaelogram + ' cm²'
+}
